7bd0a2c73be08c599e076c90c4d70f5b1a4ca24b
Rayne <rava438@student.oaisd.org>
2024-10-08 09:43:18 -0400

    fix the code
    fixed

---
+++ fix the code/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ErrorFindingExercise
+{
+    class Program
+    {
+         public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, class!");
+
+            int numberOne = 10;
+            int numberTwo = 20;
+
+            int sum = numberOne + numberTwo;
+
+            Console.WriteLine("The sum of " + numberOne + " " + "+" + " "  + numberTwo + " is: " + sum);
+        }
+    }
+}
